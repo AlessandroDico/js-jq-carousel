52dbdabd91db2b640f7b3093c1f75067b8075222
Ale <alessandrochb@gmail.com>
2020-10-27 16:30:04 +0100

    aggiunta logica per far andare le img indietro e  aggiunto 'loop'

--- assets/main.js
+++ assets/main.js
@@ -8,14 +8,32 @@ $(document).ready(function(){
     $('.right-arrow').click(function(){
         // var activeClass = $('current');
         var activeClass = $('img.current')
-        console.log(activeClass);
-
 
         activeClass.removeClass('current');
-        console.log(activeClass);
 
-        activeClass.next().addClass('current');
-        console.log(activeClass);
+/*
+QUESTO .LENGTH INDICA QUANTI ELEMENTI CI SONO ALL'INTERNO DI QUELLA VAR.. E IN QUESTO CASO POSSONO ESSERE 1 OPPURE 0.. SE è UNO ALLORA LA CONDIZIONE è VERA ALTRIMENTI è FALSA.. IN QUESTO CASO SCRIVERE (activeClass.next('img').length) EQUIVALE A SCRIVERE (activeClass.next('img').length) == 1
+*/
+        if (activeClass.next('img').length) { 
+/*equivale a (activeClass.next('img').length == 1)*/
+            activeClass.next('img').addClass('current');
+        } else {
+            $('.images-container img:first-of-type').addClass('current');
+        }
+    });
+
+    $('.left-arrow').click(function(){
+        // var activeClass = $('current');
+        var activeClass = $('img.current')
+
+        activeClass.removeClass('current');
+
+        if (activeClass.prev('img').length) {
+/*equivale a (activeClass.next('img').length == 1)*/
+            activeClass.prev('img').addClass('current');
+        } else {
+            $('.images-container img:last-of-type').addClass('current');
+        }
     });
 
 
