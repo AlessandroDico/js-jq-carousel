46500e1a233437749ef0a74fa93d30fde0066595
Ale <alessandrochb@gmail.com>
2020-10-27 16:04:27 +0100

    tolte frecce con before e after e messe come icone in html

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
+    // cliccando sulla freccia devo scorrere le immagini
 
+    // cliccando sulla freccia destra scorro le img in avanti
+
+    // cliccando sulla freccia sinistra scorro le img in dietro
+
+    $('.images-container::after').click(function(){
+        // var activeClass = $('current');
+
+
+        $('img.current').removeClass('current');
+
+        // activeClass.next().addClass('current');
+        // console.log(activeClass);
+    });
 
 
 });
