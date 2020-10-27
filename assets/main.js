753faadc7a04ede2d4f7fddc3f45b0777ad9fb69
Ale <alessandrochb@gmail.com>
2020-10-27 17:04:38 +0100

    aggiunti pallini che si muovono avanti con le immagini

--- assets/main.js
+++ assets/main.js
@@ -5,20 +5,34 @@ $(document).ready(function(){
 
     // cliccando sulla freccia sinistra scorro le img in dietro
 
+    //aggiungiamo lo spostamento dei pallini con l'immagine
+
     $('.right-arrow').click(function(){
         // var activeClass = $('current');
         var activeClass = $('img.current')
 
         activeClass.removeClass('current');
 
+        var activePoint = $('.point-switcher .fas');
+        activePoint.removeClass('fas');
+// devo togliere la classe prima dell'if perchè altrimenti togliendola direttamente nell'if rimane 'bloccata' sull' ultimo pallino quando finiscono le immagini
+
+
 /*
 QUESTO .LENGTH INDICA QUANTI ELEMENTI CI SONO ALL'INTERNO DI QUELLA VAR.. E IN QUESTO CASO POSSONO ESSERE 1 OPPURE 0.. SE è UNO ALLORA LA CONDIZIONE è VERA ALTRIMENTI è FALSA.. IN QUESTO CASO SCRIVERE (activeClass.next('img').length) EQUIVALE A SCRIVERE (activeClass.next('img').length) == 1
 */
-        if (activeClass.next('img').length) { 
+        if (activeClass.next('img').length) {
 /*equivale a (activeClass.next('img').length == 1)*/
             activeClass.next('img').addClass('current');
+
+
+            activePoint.addClass('far');
+            activePoint.next('i').addClass('fas');
+            // activePoint.next().toggleClass('fas far');
+
         } else {
             $('.images-container img:first-of-type').addClass('current');
+            $('.point-switcher i:first-child').addClass('fas');
         }
     });
 
